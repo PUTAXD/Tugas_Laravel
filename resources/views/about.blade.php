b06744f306e21dac7c1eea391a612aa541c475e8
PUTAXD <indraarvy12@gmail.com>
2024-09-08 21:57:31 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman About</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <a href="/blog">Blog</a>
+
+    <h1>Halaman About</h1>
+    <h3>Owner : <?php echo $nama; ?></h3>
+</body>
+
+</html>
---
+++ resources/views/blog.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <a href="/blog">Blog</a>
+
+    <h1 style="text-align:center">
+        Putu Indra Mahendra
+    </h1>
+
+    <article>
+        <h2>Judul 1</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sint harum assumenda aperiam repellat quidem
+            minus dolores dicta quibusdam ex minima praesentium quisquam perspiciatis ipsa possimus veritatis, dolorem
+            ipsam. Nisi voluptate ipsum totam cumque ut quaerat temporibus in, reprehenderit corporis odit saepe?
+            Aspernatur
+            eveniet quisquam quos perspiciatis omnis fugiat, quod sit tempore ab quo quae consequuntur ducimus excepturi
+            debitis architecto dolores delectus? Error quos labore ratione inventore architecto repellat! Modi autem
+            eaque
+            rerum inventore nesciunt aliquam consectetur quod omnis rem? Perspiciatis, exercitationem ratione quia
+            beatae
+            tempore mollitia! Numquam, voluptatibus. In excepturi aperiam ipsa velit aliquam voluptatum commodi labore
+            ea
+            quisquam.</p>
+    </article>
+    <article>
+        <h2>Judul 1</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa quod, debitis atque autem commodi fugiat aut
+            esse
+            eaque obcaecati velit maiores. Quas, officia accusamus aut, repellat rerum vel, perspiciatis quasi voluptas
+            eos
+            ut nulla unde odio architecto consequuntur mollitia fugit maiores aliquid deleniti officiis! Assumenda
+            repellendus provident at deserunt repudiandae!</p>
+    </article>
+</body>
+
+</html>
